e4c1c9fe8c81e7540ca7cc225a6b25c1258648c9
GOAT 🐐 <hghhbhgf114@gmail.com>
2025-05-01 17:35:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ updateClock();
 
 // Share to Friends
 function shareToFriend() {
-  const shareText = "Check out Marisel Bot! Join the channel here:\nhttps://whatsapp.com/channel/0029Vajvy2kEwEjwAKP4SI0x";
+  const shareText = "Check out Nexus Bot! Join the channel here:\nhttps://whatsapp.com/channel/0029Vajvy2kEwEjwAKP4SI0x";
   if (navigator.share) {
     navigator.share({
-      title: 'Marisel Bot',
+      title: 'Nexus-xmd',
       text: shareText,
-      url: 'https://whatsapp.com/channel/0029Vajvy2kEwEjwAKP4SI0x'
+      url: 'https://whatsapp.com/channel/0029Vad7YNyJuyA77CtIPX0x'
     });
   } else {
     prompt("Copy and share this link with your friends:", shareText);
